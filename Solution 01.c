4d35df181e3ce70dd3cb121db64d008949f23f8c
scshuvon <Shuvonchy30@gmail.com>
2025-07-24 21:28:09 +0600

    Solution 01.c

--- Solution 01.c
+++ Solution 01.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
 #include<math.h>
-
+#include<stdlib.h>
 int main()
-{
-    double lnm, td, dum;
-    double lm, dm, tr, mr;
+    {
+    double lnm, td, dum, lm, dm, tr, mr;
     int mi;
 
     printf("Enter wavelength (nm): ");
     scanf("%lf", &lnm);
 
-    if (lnm < 380 || lnm > 750)
-    {
+    if (lnm < 380 || lnm > 750) {
         printf("Out of the range. Please enter a valid number.\n");
         return 0;
     }
@@ -23,19 +21,22 @@ int main()
 
     lm = lnm * 1e-9;
     dm = dum * 1e-6;
-    tr = td * 3.1416 / 180.0;
+    tr = td * (3.1416 / 180.0);
 
     mr = (dm * sin(tr)) / lm;
-    mi = (int)(mr + 0.5);
+    mi = (int)round(mr);
 
-    if (mi == 1)
-        printf("1st order maxima\n");
-    else if (mi == 2)
-        printf("2nd order maxima\n");
-    else if (mi == 3)
-        printf("3rd order maxima\n");
-    else
+
+    int abs_m = abs(mi);
+    if (abs_m == 1) {
+        printf("%dst order maxima\n", mi);
+    } else if (abs_m == 2) {
+        printf("%dnd order maxima\n", mi);
+    } else if (abs_m == 3) {
+        printf("%drd order maxima\n", mi);
+    } else {
         printf("%dth order maxima\n", mi);
+    }
 
     return 0;
 }
